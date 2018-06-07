ab0f03e919386ba967909e834e03fcf324e4be51
steveYeah <hutchinsteve@gmail.com>
2018-06-07 20:49:02 +0100

    add size

--- art.py
+++ art.py
@@ -7,24 +7,27 @@ import numpy
 from PIL import Image
 
 
-def convert(file, outfile):
+def convert(file, outfile, width):
     image = Image.open(file.name).convert('L')
     data = numpy.array(image, dtype="int32" )
     import pdb; pdb.set_trace()
+
+    # width of the matrix / width
 
 
 def main(argv):
     parser = argparse.ArgumentParser(
         description="ASCII YEAHHH",
-        )
+    )
 
     parser.add_argument('file', type=argparse.FileType('r'))
     parser.add_argument('outfile', type=argparse.FileType('w'))
-    
+    parser.add_argument('--width', type=int, default=78)
+
 
     parsed = parser.parse_args()
 
-    return convert(parsed.file, parsed.outfile)
+    return convert(parsed.file, parsed.outfile, width)
 
 
 if __name__ == "__main__":
